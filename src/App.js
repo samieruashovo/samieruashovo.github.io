7dd30371f6b2354044c1d1256a950a7018b81ec3
Samier Uddin Ahammad Shovo <samieruashovo@gmail.com>
2025-12-26 00:32:45 +0600

    need to add terms of service and home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SlideFinityPrivacyPolicy from "./components/privacy-policy/SlideFinityPrivacyPolicy";
 
@@ -9,6 +9,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/slide-finity-privacy-policy" element={<SlideFinityPrivacyPolicy />} />
+        <Route path="/slide-finity-terms-of-service" element={<SlideFinityPrivacyPolicy />} />
+        <Route path="/slide-finity-home-page" element={<SlideFinityPrivacyPolicy />} />
+
       </Routes>
     </Router>
   );
